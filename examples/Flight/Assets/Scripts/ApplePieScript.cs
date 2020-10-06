df56d4d64c07628ed3c7ae4dff9f11f5bebcb007
Mike Treanor <micitari@gmail.com>
2020-10-05 20:29:12 -0400

    Added Flight simulator example

---
+++ examples/Flight/Assets/Scripts/ApplePieScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ApplePieScript : MonoBehaviour
+{
+	GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	// Because this apple apie was is instantiated at runtime, we need to dynamically 
+    	// get the reference ot the game manager.
+    	//
+    	// NOTE: This way to finding a game object and then immediately get a component on it isn't
+    	// "safe" because if GameObject.Find fails (returns null), we immediately attempt to call
+    	// GetComponent on null. This will produce a runtime error. So only use this pattern when you
+    	// are 100% sure that there is a GameObject with the name you are searching for.
+		gm = GameObject.Find("GameManagerObj").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter(Collider other)
+	{
+		// When we collide with the apple tree, update score in game manager, and destroy the tree.
+		if (other.CompareTag("appleTree")) {
+			Destroy(other.gameObject);
+			gm.IncreaseScore();
+		}
+	}
+}
---
+++ examples/Flight/Assets/Scripts/PlaneController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaneController : MonoBehaviour
+{
+	public Rigidbody rb;
+
+	public GameObject applePiePrefab;
+	
+	float speed = 10;
+	float maxSpeed = 20;
+	float forwardSpeed = 1;
+	float pitchSpeed = 80;
+	float pitchModSpeedRate = 8f;
+	float rollSpeed = 80;
+	
+    // Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		float hAxis = Input.GetAxis("Horizontal");
+		float vAxis = Input.GetAxis("Vertical");
+
+		// Rotate the plane based on input.
+		float xRot = vAxis * pitchSpeed * Time.deltaTime;
+		float yRot = hAxis * rollSpeed / 4 * Time.deltaTime;
+		float zRot = -hAxis * rollSpeed * Time.deltaTime;
+		transform.Rotate(xRot, yRot, zRot, Space.Self);
+
+		// Compute a modifier (forwardSpeed) based on if the plane is looking up or down.
+		// If the y value of the tranform's forward is positive, we know we are looking up, 
+		// if it is negative, we know we are looking down.
+		forwardSpeed += -transform.forward.y * pitchModSpeedRate * Time.deltaTime;
+		// Make sure we never to too fast and that forwardSpeed never goes below zero.
+		forwardSpeed = Mathf.Clamp(forwardSpeed, 0, 5f);
+
+		// Move forward as modified by forwardSpeed and speed.
+		transform.Translate(transform.forward * speed * forwardSpeed * Time.deltaTime, Space.World);
+
+		// If we ever go too slow, turn on the gravity on the rigid body so that the plane falls.
+		if (forwardSpeed <= 0.1f) {
+			rb.isKinematic= false;
+			rb.useGravity = true;
+		}
+
+		// Make sure we never go below the ground. First, we find out what the height of the terrain is at
+		// the position the plane is in. If the plane's y position is below that position, we know we have gone
+		// too low. In the if statement, we simply place the plane at the height of the terrain.
+		// But this is where you could have the plane crash, or have it slow down, or something.
+		float terrainHeight = Terrain.activeTerrain.SampleHeight(transform.position);
+		if (transform.position.y < terrainHeight) {
+			transform.position = new Vector3(transform.position.x, terrainHeight, transform.position.z);
+		}
+
+		// Shoot an apple pie whenever the player presses space.
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			GameObject applePie = Instantiate(applePiePrefab, transform.position + transform.forward * 3, Quaternion.identity);
+			Rigidbody applePieRB = applePie.GetComponent<Rigidbody>();
+			applePieRB.AddForce(transform.forward * 8000);
+			Destroy(applePie, 5);
+		}
+		
+    	// Position the camera behind and above the player.
+		Vector3 cameraPosition = transform.position - transform.forward * 12 + Vector3.up * 5;
+		Camera.main.transform.position = cameraPosition;
+		Vector3 lookAtPos = transform.position + transform.forward * 8;
+		
+		// Rotate the camera so that it looks always in front of the plane.
+		Camera.main.transform.LookAt(lookAtPos, Vector3.up);
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("ring")) {
+			speed = Mathf.Clamp(speed + 5, 0, maxSpeed);
+		}
+	}
+}
